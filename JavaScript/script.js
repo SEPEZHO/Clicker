8121324555837d2c860b60b90d9bb9250fc37558
SEPEZHO <47150504+SEPEZHO@users.noreply.github.com>
2019-05-20 13:47:09 +0300

    Completed the menu

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,16 +1,74 @@
-function menuAnim(argument) {
+function menuAnim() {
 	$('#inventory').click(beginInventoryAnimate);
 	$('#subInventory').mouseout(endInventoryAnimate);
-	$('#subInventory').hide();
-
+	$('#shop').click(beginShopAnimate);
+	$('#subShop').mouseout(endShopAnimate);
+	$('#settings').click(beginSettingsAnimate);
+	$('#subSettings').mouseout(endSettingsAnimate);
 }
 function beginInventoryAnimate(){
-console.log('Begin')
-$('#subInventory').show();
-
+	$('#subInventory').css({
+		zIndex: 2
+	})
+	$('#subInventory').animate({
+		height: '100%'
+	},{
+		duration: 10
+	})
+	endShopAnimate();
+	endSettingsAnimate();
 }
 function endInventoryAnimate(){
-console.log('End')
-$('#subInventory').hide();
-
+	$('#subInventory').css({
+		zIndex: 0
+	})
+	$('#subInventory').animate({
+		height: '0%'
+	},{
+		duration: 10
+	})
+}
+function beginShopAnimate(){
+	$('#subShop').css({
+		zIndex: 2
+	})
+	$('#subShop').animate({
+		height: '100%'
+	},{
+		duration: 10
+	})
+	endInventoryAnimate();
+	endSettingsAnimate();
+}
+function endShopAnimate(){
+	$('#subShop').css({
+		zIndex: 0
+	})
+	$('#subShop').animate({
+		height: '0%'
+	},{
+		duration: 10
+	})
+}
+function beginSettingsAnimate(){
+	$('#subSettings').css({
+		zIndex: 2
+	})
+	$('#subSettings').animate({
+		height: '100%'
+	},{
+		duration: 10
+	})
+	endInventoryAnimate();
+	endShopAnimate();
+}
+function endSettingsAnimate(){
+	$('#subSettings').css({
+		zIndex: 0
+	})
+	$('#subSettings').animate({
+		height: '0%'
+	},{
+		duration: 10
+	})
 }
