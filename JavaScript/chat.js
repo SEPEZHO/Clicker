c8e19cd443cab1d5246f675d276b065ae5bd3b37
SEPEZHO <47150504+SEPEZHO@users.noreply.github.com>
2019-08-15 16:34:33 +0300

    Я запилил чат на wss.
    Выровнял код.

--- JavaScript/chat.js
+++ JavaScript/chat.js
@@ -7,27 +7,21 @@ function chatBegin() {
             $('#chat').hide();
         })
     });
-        
+
     var socket = new WebSocket('wss://sepezho.ru:8080/php/chat.php')
 
     socket.onopen = function() {
         messageInfo("Соединение установлено.");
-        let chatTextSndObj={
-                  chat_user: 'login',
-                    chat_lvl: 'lvl',
-                    chat_message: 'chatTextSnd'
-        };
-        socket.send(JSON.stringify(chatTextSndObj));
     };
     socket.onclose = function(event) {
-        messageInfo('Соединение закрыто');
+        messageInfo('Соединение закрыто.');
     };
     socket.onmessage = function(event) {
-        var data = JSON.parse(event.data)
+        var data = JSON.parse(event.data);
         message(data.message, data.user, data.lvl, data.date);
     };
     socket.onerror = function(error) {
-        messageInfo("Ошибка " + error.message);
+        messageInfo('Произошла ошибка.');
     };
 
     $('#chatButtonSnd').click(() => {
@@ -56,17 +50,16 @@ function chatBegin() {
             cAlert('Вам следует войти или зарегестрироваться.')
         }
     })
-    var messages = 0;
 }
 
 function message(text, name, lvl, time) {
     var block = "<div class='chatMess'><div class='chat_message'>" + text + "</div><div class='infoChat'><div class='name'>" + name + "(" + lvl + ")</div><div class='time'>" + time + "</div></div></div>";
     $('#messageBox').append(block);
-    // setTimeout(() => { $('#messageBox').children(":first").first().remove() }, 30000);
+    setTimeout(() => { $('#messageBox').children(":first").first().remove() }, 30000);
 }
 
 function messageInfo(text) {
     var infoblock = "<div class='chatMess'><div class='chat_message_info'>" + text + "</div></div>";
     $('#messageBox').append(infoblock);
-    // setTimeout(() => { $('#messageBox').children(":first").first().remove() }, 30000);
+    setTimeout(() => { $('#messageBox').children(":first").first().remove() }, 30000);
 }
--- php/chat.php
+++ php/chat.php
@@ -5,31 +5,26 @@ use Workerman\Worker;
 // SSL context.
 $context = array(
     'ssl' => array(
-      'local_cert'=> '/home/s50962/ssl/certs/sepezho_ru_c53de_67889_1573308743_9bc6cca7fd85882a823551e2561ee831.crt',
+        'local_cert'=> '/home/s50962/ssl/certs/sepezho_ru_c53de_67889_1573308743_9bc6cca7fd85882a823551e2561ee831.crt',
         'local_pk' => '/home/s50962/ssl/keys/c53de_67889_a3169f1d541de2e450c39cd7959c742c.key',
-       'verify_peer'  => false,
+        'verify_peer'  => false,
     )
 );
 
-
 // Create a Websocket server with ssl context.
 $ws_worker = new Worker("websocket://0.0.0.0:8080", $context);
-
 
 // Enable SSL. WebSocket+SSL means that Secure WebSocket (wss://). 
 // The similar approaches for Https etc.
 $ws_worker->transport = 'ssl';
 
 // Emitted when new connection come
-$ws_worker->onConnect = function($connection)
-{
+$ws_worker->onConnect = function($connection){
     echo "New connection\n";
-// print_r (Worker::getAllWorkers());
 };
 
-$ws_worker->onMessage = function($connection, $data) use ($ws_worker)
-{
-    // Send hello $data
+$ws_worker->onMessage = function($connection, $data) use ($ws_worker){
+    
     $data = json_decode($data);
     $date = date("H:i");
     $messageArray = [
@@ -37,53 +32,18 @@ $ws_worker->onMessage = function($connection, $data) use ($ws_worker)
         'lvl' => $data->chat_lvl,
         'message' => $data->chat_message,
         'date' => $date
-        ];
+    ];
+    
     // отправляем сообщение
     foreach ($ws_worker->connections as $connect) {
        $connect->send(json_encode($messageArray));
     }
 };
+
 // Emitted when connection closed
-$ws_worker->onClose = function($connection)
-{
+$ws_worker->onClose = function($connection){
     echo "Connection closed\n";
 };
 
 Worker::runAll();
 
-
-
-// require_once ('../vendor/autoload.php');
-// use Workerman\Worker;
-// use Workerman\Connection\AsyncTcpConnection;
-
-// $worker = new Worker();
-// $worker->onWorkerStart = function()
-// {
-//     $context = array(
-//     'ssl' => array(
-//       'local_cert'=> '/home/s50962/ssl/certs/sepezho_ru_c53de_67889_1573308743_9bc6cca7fd85882a823551e2561ee831.crt',
-//         'local_pk' => '/home/s50962/ssl/keys/c53de_67889_a3169f1d541de2e450c39cd7959c742c.key',
-//         'verify_peer' => false
-//         )
-//     );
-
-//     $ws_connection = new Worker("websocket://0.0.0.0:8080", $context);
-
-//     $ws_connection->transport = 'ssl';
-
-//     $ws_connection->onConnect = function($connection){
-//         $connection->send('hello');
-//     };
-//     $ws_connection->onMessage = function($connection, $data){
-//         echo "recv: $data\n";
-//     };
-//     $ws_connection->onError = function($connection, $code, $msg){
-//         echo "error: $msg\n";
-//     };
-//     $ws_connection->onClose = function($connection){
-//         echo "connection closed\n";
-//     };
-//     // $ws_connection->connect();
-// };
-// Worker::runAll();
